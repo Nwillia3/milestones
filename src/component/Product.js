10b40a7d9c5e372bf7b6dc31950804e0fe616440
nodox <snatera09@gmail.com>
2019-08-31 14:40:27 -0500

    feat: styled form and data

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,14 +1,7 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import uuid from "uuid/v1";
-import Milestone from "./milestone";
+import Moment from "react-moment";
 import { getMileStones, addMileStone } from "../data/milestones";
-import Form from "./Form";
-
-// NOTE: Need this version of import for tailwind to work
-
-// eslint-disable-next-line
-
-import Modal from "./Modal";
 
 function Product() {
   const [data, setData] = useState(getMileStones());
@@ -22,15 +15,15 @@ function Product() {
     date: Date.now()
   });
 
-  const { title, description } = formData;
-
   const onChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const onSubmit = e => {
     e.preventDefault();
+
     addMileStone(formData);
+
     setData(getMileStones());
   };
 
@@ -41,78 +34,142 @@ function Product() {
     toggleData
       ? (mileStones = data.sort((a, b) => (a.date < b.date ? 1 : -1)))
       : (mileStones = data.sort((a, b) => (a.date > b.date ? 1 : -1)));
-    setData(mileStones);
     setToggleData(!toggleData);
   };
 
-  const handleClick = e => {
-    sortByDate();
-  };
 
-  const onKeyUp = e => {
-    //get value of input
-    let val = e.target.value;
-    let goals = data;
+  if (data === null || data === undefined || !data) {
+    displayData = [];
+  }
+  
+  let displayData = data.map(milestone => (
+    <div key={milestone.title} class="max-w-sm mx-auto my-10 rounded overflow-hidden shadow-lg">
+      <div class="px-6 py-4">
+        <div class="py-4 font-thin">
+          <Moment format="YYYY/MM/DD">{milestone.date}</Moment>
+        </div>
 
-    val === ""
-      ? (goals = data)
-      : (goals = data.filter(goal => goal.title === e.target.value));
-
-    console.log(goals);
-    setData(goals);
-  };
-
-  const [modal, setModal] = useState(false);
+        <div class="font-bold text-xl mb-2">
+          {milestone.title}
+        </div>
+        <p class="text-gray-700 text-base">
+          {milestone.description}
+        </p>
+      </div>
+      <div class="px-6 py-4">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+      </div>
+    </div>
+  ));
 
   return (
-    <section>
-
-
-        <button onClick={e => handleClick(e)}>
-          {toggleData ? "Newest" : "Oldest"}
-        </button>        
-
-        <input type="text" onKeyUp={e => onKeyUp(e)} />
-
-        <Form
-          onChange={e => onChange(e)}
-          onSubmit={e => onSubmit(e)}
-          title={title}
-          description={description}
-        />
-
-        <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-          <div class="flex items-center flex-shrink-0 text-white mr-6">
-            <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-            <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
-          </div>
-          <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-            </button>
-          </div>
-          <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-            <div class="text-sm lg:flex-grow">
-              <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                Docs
-              </a>
-              <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                Examples
-              </a>
-              <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-                Blog
-              </a>
-            </div>
-            <div>
-              <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
-            </div>
-          </div>
-        </nav>
-
-        <div>
-          <Milestone milestones={data} />
+    <div>
+      <nav class="flex items-center justify-between flex-wrap bg-black p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <span class="font-semibold text-xl tracking-tight">Milestones</span>
         </div>
-    </section>
+        <div class="block lg:hidden">
+          <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+          </button>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow">
+            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Home
+            </a>
+          </div>
+        </div>
+      </nav>
+
+      <div class="container mx-auto my-8">
+        <form class="w-full max-w-lg mx-auto" onSubmit={e => onSubmit(e)}>
+          <div class="flex flex-wrap -mx-3 mb-2">
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                Month
+              </label>
+              <div class="relative">
+                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                Day
+              </label>
+              <div class="relative">
+                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+              </div>
+
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                Year
+              </label>
+              <div class="relative">
+                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                  <option>2019</option>
+                  <option>2018</option>
+                  <option>2017</option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="goal-title">
+                Goal
+              </label>
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                id="goal-title" 
+                type="text"
+                name="title"
+                onChange={e => onChange(e)}
+                placeholder="What do you want to accomplish?" />
+            </div>
+
+            <div class="w-full px-3">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="goal-description">
+                Description
+              </label>
+              <textarea class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                id="goal-description" 
+                type="text"
+                name="description"
+                onChange={e => onChange(e)}
+                placeholder="How will you accomplish this? Why do you want to do this?" 
+              ></textarea>
+            </div>
+          </div>
+          <button class="bg-gray-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+            Add milestone
+          </button>
+        </form>
+      </div>
+
+      <div class="container mx-auto my-8 w-full">
+        {displayData}
+      </div>
+    </div>
   );
 }
 
